5ebdf58056beeeafc785a0af407caaa039c9fd4e
abedihamid257-afk <abedihamid257@gmail.com>
2026-08-18 23:16:57 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,9 @@
-'use strict';
-
 const frame = document.getElementById('googleFrame');
 
-// ===== رفتن به خانه =====
 function goHome() {
     frame.src = 'https://www.google.com/webhp?igu=1';
 }
 
-// ===== رفرش =====
 function reloadFrame() {
     frame.src = frame.src;
 }
-
-// ===== تب جدید (پنجره جدید) =====
-function openNewTab() {
-    window.open('https://www.google.com/', '_blank');
-}
-
-// ===== اگه گوگل لود نشد =====
-frame.addEventListener('error', () => {
-    frame.src = 'https://duckduckgo.com/';
-});
