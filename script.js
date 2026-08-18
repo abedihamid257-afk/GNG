619f8525c99665ff7a6b099f2b55cadbc0671868
abedihamid257-afk <abedihamid257@gmail.com>
2026-08-18 23:36:00 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,9 @@
 const frame = document.getElementById('googleFrame');
-const overlay = document.getElementById('gngOverlay');
 
 function goHome() {
     frame.src = 'https://www.google.com/webhp?igu=1';
-    showOverlay();
 }
 
 function reloadFrame() {
     frame.src = frame.src;
 }
-
-function hideOverlay() {
-    overlay.classList.add('hidden');
-}
-
-function showOverlay() {
-    overlay.classList.remove('hidden');
-}
-
-// وقتی گوگل لود شد، لوگو رو مخفی کن
-frame.addEventListener('load', () => {
-    setTimeout(() => {
-        hideOverlay();
-    }, 1000);
-});
-
-// اول صفجه لوگوی GNG نشون بده
-showOverlay();
